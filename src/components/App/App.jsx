623316de8a6dce97a38ca32cb10b67cd7d8a9288
dnPC <denisagafonovwork.gmail.com>
2020-06-27 10:45:59 +0300

    refactor: add wrapper for all components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from '../header/header';
 import DividerGray from '../divider/_gray/divider_gray';
 import Me from '../me/me';
+import AboutMe from '../about-me/about-me';
 
 import './app.scss';
-import AboutMe from '../about-me/about-me';
+import '@scss/general.scss';
 
 const App = () => {
     return(
--- src/components/me/__photo/me__photo.jsx
+++ src/components/me/__photo/me__photo.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './me__photo.scss';
 
-const MePhoto = () => <article className="me__photo"></article>;
+const MePhoto = () => <article className="me__photo"><div className="wrapper"></div></article>;
 
 export default MePhoto;
