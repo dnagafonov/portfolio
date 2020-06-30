5afa52eac155ad8a057ad5ee458f3a1a525756e7
dnPC <denisagafonovwork.gmail.com>
2020-06-30 14:24:27 +0300

    feat: add optimization using React.lazy

--- src/components/technology/technology.jsx
+++ src/components/technology/technology.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
-import TechnologyPhoto from './__photo/technology__photo';
 import TechnologyName from './__name/technology__name';
 import Rate from '../rate/rate';
-
+import Note from "../note/note";
 import './technology.scss';
+
+const TechnologyPhoto = React.lazy(() => import('./__photo/technology__photo'))
 
 const Technology = ({name, stars, path}) => (
     <article className="technology">
-        <TechnologyPhoto name={name} path={path} />
+        <Suspense fallback={<Note>Loading...</Note>}><TechnologyPhoto name={name} path={path} /></Suspense>
         <TechnologyName name={name} />
         <Rate stars={stars}/>
     </article>
